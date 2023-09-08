3276b03f02cb0f8aa6abf265f910a5bd2722d85a
Alex Dreier <amdreier@gmail.com>
2023-09-08 16:56:41 -0400

    add node internal security

--- node/src/index.js
+++ node/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { readFile } = require('fs');
 const { exec } = require("child_process");
 const bodyParser = require('body-parser');
+const e = require('express');
 require('dotenv').config()
 const app = express();
 const port = 3000;
@@ -44,21 +45,32 @@ app.get('/nodeSubfolder', (request, response) => {
 });
 
 app.get('/creds', (request, response) => {
-    response.send(process.env.ROOT_PASS);
-    log("GET /creds", request, "Success");
+    let req_ip = request.socket.remoteAddress;
+
+    if (req_ip == '10.0.0.40') {
+        response.send(process.env.ROOT_PASS);
+        log("GET /creds", request, "Allowed");
+    } else {
+        log("GET /creds", request, "Denied");
+    }
 });
 
 app.post('/allow', (req, res) => {
     let addr = req.body.addr;
     let user = req.body.user;
+    let req_ip = req.socket.remoteAddress;
 
-    let command = `echo "${process.env.ROOT_PASS}" | sudo -S ufw insert 1 allow from ${addr} proto tcp to any port 25565 comment '${user}'`;
-    exec(command);
+    if (req_ip == '10.0.0.40') {
 
-    res.send("true");
+        let command = `echo "${process.env.ROOT_PASS}" | sudo -S ufw insert 1 allow from ${addr} proto tcp to any port 25565 comment '${user}'`;
+        exec(command);
 
-    // res.send(`Done: ${addr}\n`);
-    log(`POST ${addr} /allow`, req, `Added: ${addr}, ${user}`);
+        res.send("true");
+
+        log(`POST ${addr}, ${user} /allow`, req, `Added: ${addr}, ${user}`);
+    } else {
+        log(`POST ${addr}, ${user} /allow`, req, `Denied`);
+    }
 });
 
 app.listen(port, () => console.log(`Server started on port: ${port}\n`));
