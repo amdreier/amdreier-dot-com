34d2630c9b21f4a923801a1f9addd49f2da2199e
Alex Dreier <amdreier@gmail.com>
2023-09-08 20:28:41 -0400

    decouple node servers

---
+++ node/web/src/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { readFile } = require('fs');
+const { exec } = require("child_process");
+const bodyParser = require('body-parser');
+require('dotenv').config()
+const app = express();
+const port = 3000;
+
+function log(type, request, response) {
+    console.log(`IP: ${request.header('x-forwarded-for')}`);
+    console.log(`Request: ${type}`);
+    console.log(`Response: ${response}`);
+    console.log("");
+};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.get('/', (request, response) => {
+    readFile('/var/www/node/src/html/nodeTest.html', 'utf8', (err, html) => {
+        if (err) {
+            console.log(err);
+            response.status(500).send('server error\n');
+        }
+
+        response.send(html);
+
+        log("GET /", request, "Success");
+    });
+});
+
+app.get('/nodeSubfolder', (request, response) => {
+    readFile('/var/www/node/src/html/nodeSubfolder.html', 'utf8', (err, html) => {
+        if (err) {
+            console.log(err);
+            response.status(500).send('server error\n');
+        }
+
+        response.send(html);
+
+        log("GET /nodeSubfolder", request, "Success");
+    });
+});
+
+app.listen(port, () => console.log(`Server started on port: ${port}\n`));
